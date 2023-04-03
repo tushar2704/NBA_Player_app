0c12f5b9f986b82262c63660437702f4e7e76f72
tushar2704 <tushar.inseec@gmail.com>
2023-04-03 12:03:40 +0530

    app

---
+++ NBA_app.py
@@ -0,0 +1,19 @@
+## NBA Player App by github.com/tushar2704
+
+# Importing required libraries
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+# Streamlit App body
+st.title("🏀NBA Players Explorer🏀")
+st.write(""" ##### 🏀By Tushar Aggarwal🏀
+""")
+
+st.markdown("""
+This app perform webscraping of NBA Player Stats Data!
+""")
---
+++ README.md
@@ -0,0 +1,12 @@
+# Tushar Agagrwal
+## NBA Player Stats Explorer
+```
+streamlit run NBA_app.py
+```
+ 
+ # Deployed Link:
+ ```
+ 1
+ ```
+
+ ## Follow me on kaggle.com/tusharaggarwal27
